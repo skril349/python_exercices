b12971ee770e9906e26086b0082352fc449e4426
skril349 <tvives.cabaleiro@gmail.com>
2025-10-29 14:17:26 +0100

    adding link to go to detail view

--- day16/src/my_project/base/urls.py
+++ day16/src/my_project/base/urls.py
@@ -3,5 +3,5 @@ from .views import ListaPendientes, DetalleTarea
 
 urlpatterns = [
     path("",ListaPendientes.as_view(),name="pendientes"),
-    path("tarea/<int:pk>",DetalleTarea.as_view(),name="detalles"),
+    path("tarea/<int:pk>/",DetalleTarea.as_view(),name="tarea"),
 ]
