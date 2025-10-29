d0fc6c54ab4295f325898d9c0ef3659a4ff432d4
skril349 <tvives.cabaleiro@gmail.com>
2025-10-29 17:53:54 +0100

    adding form input crear-tareas

--- day16/src/my_project/base/urls.py
+++ day16/src/my_project/base/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ListaPendientes, DetalleTarea
+from .views import ListaPendientes, DetalleTarea,CrearTarea
 
 urlpatterns = [
-    path("",ListaPendientes.as_view(),name="pendientes"),
+    path("",ListaPendientes.as_view(),name="tareas"),
     path("tarea/<int:pk>/",DetalleTarea.as_view(),name="tarea"),
+    path("crear-tarea/",CrearTarea.as_view(),name="crear-tarea"),
 ]
--- day16/src/my_project/base/views.py
+++ day16/src/my_project/base/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from .models import Tarea
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
 # Create your views here.
 
 class ListaPendientes(ListView):
@@ -12,3 +15,10 @@ class DetalleTarea(DetailView):
     model = Tarea
     context_object_name = "tarea"
     template_name="base/tarea.html"
+
+class CrearTarea(CreateView):
+    model = Tarea
+    fields = "__all__"
+    success_url = reverse_lazy("tareas")
+
+
