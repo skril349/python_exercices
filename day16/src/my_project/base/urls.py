34988e0e3067cb447c4a0ed7be9842f570727a46
skril349 <tvives.cabaleiro@gmail.com>
2025-10-29 13:40:29 +0100

    creating detail view

--- day16/src/my_project/base/urls.py
+++ day16/src/my_project/base/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ListaPendientes
+from .views import ListaPendientes, DetalleTarea
 
 urlpatterns = [
-    path("",ListaPendientes.as_view(),name="pendientes")
+    path("",ListaPendientes.as_view(),name="pendientes"),
+    path("tarea/<int:pk>",DetalleTarea.as_view(),name="detalles"),
 ]
--- day16/src/my_project/base/views.py
+++ day16/src/my_project/base/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Tarea
 # Create your views here.
 
 class ListaPendientes(ListView):
     model = Tarea
     context_object_name="tareas"
+
+class DetalleTarea(DetailView):
+    model = Tarea
+    context_object_name = "detalles"
