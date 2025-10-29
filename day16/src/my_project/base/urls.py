5bd5f55c276fe0f95a2a07b432c4a23a1f4b8ba8
skril349 <tvives.cabaleiro@gmail.com>
2025-10-29 18:24:10 +0100

    editar-tarea url and view

--- day16/src/my_project/base/urls.py
+++ day16/src/my_project/base/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ListaPendientes, DetalleTarea,CrearTarea
+from .views import ListaPendientes,DetalleTarea,CrearTarea,EditarTarea
 
 urlpatterns = [
     path("",ListaPendientes.as_view(),name="tareas"),
     path("tarea/<int:pk>/",DetalleTarea.as_view(),name="tarea"),
     path("crear-tarea/",CrearTarea.as_view(),name="crear-tarea"),
+    path("editar-tarea/<int:pk>/",EditarTarea.as_view(),name="editar-tarea"),
+
 ]
--- day16/src/my_project/base/views.py
+++ day16/src/my_project/base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from .models import Tarea
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView,UpdateView
 from django.urls import reverse_lazy
 
 # Create your views here.
@@ -17,6 +17,11 @@ class DetalleTarea(DetailView):
     template_name="base/tarea.html"
 
 class CrearTarea(CreateView):
+    model = Tarea
+    fields = "__all__"
+    success_url = reverse_lazy("tareas")
+
+class EditarTarea(UpdateView):
     model = Tarea
     fields = "__all__"
     success_url = reverse_lazy("tareas")
