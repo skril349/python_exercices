1d0e4531ef3c5019b7407c92d3a474f08dd33f29
skril349 <tvives.cabaleiro@gmail.com>
2025-10-29 10:00:06 +0100

    using shutil to move files

---
+++ day9/shutil_os.py
@@ -0,0 +1,11 @@
+import os 
+import shutil
+
+print(os.getcwd())
+
+# archivo = open("curso.txt","w")
+# archivo.write("texto de prubea")
+# archivo.close()
+
+#mover archivos
+shutil.move("curso.txt", "C:\\Users\\antoni\\Documents\\Udemy\\python_exercices")
