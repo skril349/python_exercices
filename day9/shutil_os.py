66448d6f27474dabccf9c83db9e1323a3eb8c40b
skril349 <tvives.cabaleiro@gmail.com>
2025-10-29 10:07:45 +0100

    read folders, subfolders and files with send2trash

--- day9/shutil_os.py
+++ day9/shutil_os.py
@@ -1,5 +1,6 @@
 import os 
 import shutil
+import send2trash
 
 print(os.getcwd())
 
@@ -8,4 +9,15 @@ print(os.getcwd())
 # archivo.close()
 
 #mover archivos
-shutil.move("curso.txt", "C:\\Users\\antoni\\Documents\\Udemy\\python_exercices")
+#shutil.move("curso.txt", "C:\\Users\\antoni\\Documents\\Udemy\\python_exercices")
+ruta = "C:\\Users\\antoni\\Documents\\Udemy"
+
+for carpeta,subcarpeta,archivo in os.walk(ruta):
+    print(f"En la carpeta: {ruta}")
+    print(f"Las subcarpetas son")
+    for sub in subcarpeta:
+        print(f"\t{sub}")
+    print("Los archivos son")
+    for arch in archivo:
+        print(f"\t{arch}")
+    print("\n")
